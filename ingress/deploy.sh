0ab51910b6b6fa42086d99d66c06df0a64a4baea
raslenraslen <raslenm39@gmail.com>
2025-09-17 20:18:17 +0000

    xd

---
+++ ingress/deploy.sh
@@ -0,0 +1,9 @@
+ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.0/deploy/static/provider/baremetal/deploy.yaml
+
+ 
+
+  kubectl get pods -n ingress-nginx --watch
+
+
+sudo nano /etc/hosts
+
--- vm-vierge/prerequis.sh
+++ vm-vierge/prerequis.sh
@@ -1,1 +1,6 @@
 sudo chown -R raslen:raslen ~/tutenv
+
+kubectl taint nodes raslen node-role.kubernetes.io/control-plane:NoSchedule-
+
+
+kubectl port-forward --address 0.0.0.0 svc/productpage 9080:9080
